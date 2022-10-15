b477f37ef713ae931c139432f055c4595722e361
FelipeOthon <felipeosantiago@gmail.com>
2022-10-14 22:54:55 -0300

    enviar readme

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+        float n1,n2,n3,n4;
+
+        System.out.println("digite a primeira nota: ");
+        n1=sc.nextFloat();
+        System.out.println("digite a segunda nota: ");
+        n2= sc.nextFloat();
+        System.out.println("digite a terceira nota: ");
+        n3 =  sc.nextFloat();
+        System.out.println("digite a quarta  nota: ");
+        n4 = sc.nextFloat();
+
+        float x = n1 + n2 + n3 + n4 /4;
+        System.out.println("a media das notas foi: " + x );
+
+        if (x >= 70) {
+            System.out.println("Aprovado");
+
+        } else { System.out.println("Reaprovado");
+
+        }
     }
 }
